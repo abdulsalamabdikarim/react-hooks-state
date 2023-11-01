22e0ecc147c75530574546bfb24ce239dc8ff2c1
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-11-01 21:40:25 +0300

    Completed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,7 @@ function Counter() {
 
   function increment() {
     setCount(count + 1);
+    //setCount((count)=> count + 1);   also okay(better in this case) using the callback syntax
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
